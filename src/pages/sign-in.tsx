84f2c6c5b7dfbad2f5e777cad7f6c6fcd39f9bf2
Paul George <paultibulca@gmail.com>
2023-07-22 00:02:34 +0300

    sign in page

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -15,19 +15,14 @@ const SignInPage: NextPage = (_) => {
     <>
       <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
+          <div
+            className="absolute inset-0 bg-zinc-900 bg-cover bg-center bg-no-repeat"
+            style={{
+              backgroundImage: `url(https://images.unsplash.com/photo-1549317336-206569e8475c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80)`,
+            }}
+          />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <Command className="mr-2 h-6 w-6" /> My Netwrk
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
           </div>
         </div>
         <div className="lg:p-8">
