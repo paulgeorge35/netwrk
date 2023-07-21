bcb7bd296a403a912f71486d75cd5eaf1ce78d82
Paul George <paultibulca@gmail.com>
2023-07-21 17:13:17 +0300

    page headers

--- src/pages/reminders.tsx
+++ src/pages/reminders.tsx
@@ -1,11 +1,11 @@
-import { type NextPage } from 'next'
-import Head from 'next/head'
-import { useSession } from 'next-auth/react'
+import { type NextPage } from 'next';
+import Head from 'next/head';
+import { useSession } from 'next-auth/react';
 
-import { Sidebar } from '@/components/sidebar'
+import { Sidebar } from '@/components/sidebar';
 
 const Reminders: NextPage = (_) => {
-  const session = useSession()
+  const session = useSession();
   return (
     <>
       <Head>
@@ -15,10 +15,25 @@ const Reminders: NextPage = (_) => {
       </Head>
       <main className="grid min-h-screen bg-background lg:grid-cols-5">
         <Sidebar className="hidden lg:block" userId={session.data?.user.id} />
-        <div className="text-secondary-foreground col-span-3 lg:col-span-4 lg:border-l"></div>
+        <div className="col-span-3 px-4 text-secondary-foreground lg:col-span-4 lg:border-l">
+          <span className="flex items-center justify-between gap-2">
+            <span className="flex flex-col gap-1 pt-4">
+              <span className="flex items-center gap-6">
+                <h1>⏰</h1>
+                <h1 className="flex max-w-xs items-center justify-center rounded-lg border-transparent py-2 text-2xl font-bold shadow-none">
+                  Reminders
+                </h1>
+              </span>
+              <h1 className="flex max-w-md items-center justify-center rounded-lg border-transparent py-1 text-sm text-gray-500 shadow-none">
+                All of your Reminders
+              </h1>
+            </span>
+            {/* <AddContactSheet /> */}
+          </span>
+        </div>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Reminders
+export default Reminders;
