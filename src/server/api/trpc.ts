8a2edd48caaf45df78154c3e6f24c3505ec1ea9f
Paul George <paultibulca@gmail.com>
2023-07-22 15:49:24 +0300

    cleared console

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -109,7 +109,6 @@ export const publicProcedure = t.procedure;
 /** Reusable middleware that enforces users are logged in before running the procedure. */
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
   if (!ctx.session || !ctx.session.user) {
-    console.log(ctx.session);
     throw new TRPCError({ code: 'UNAUTHORIZED' });
   }
   return next({
