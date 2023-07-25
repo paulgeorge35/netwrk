281c812f837bd8164e715285f0d45e48770ca7a7
Paul George <paultibulca@gmail.com>
2023-07-26 01:46:35 +0300

    hidden shortcut keys on mobile

--- src/components/ui/shortcut-key.tsx
+++ src/components/ui/shortcut-key.tsx
@@ -9,7 +9,7 @@ export const ShortcutKeys = ({
 }) => (
   <span
     className={cn(
-      'min-w-5 flex h-5 items-center justify-center rounded border px-1 font-mono text-xs',
+      'min-w-5 hidden h-5 items-center justify-center rounded border px-1 font-mono text-xs sm:flex',
       square ? 'aspect-square' : 'aspect-auto'
     )}
   >
