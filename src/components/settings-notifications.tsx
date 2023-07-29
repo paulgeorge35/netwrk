8c0c1b87ecf979c5fd4760d2039b15db7ea94d22
Paul George <paultibulca@gmail.com>
2023-07-29 13:32:05 +0300

    notifications settings done

--- src/components/settings-notifications.tsx
+++ src/components/settings-notifications.tsx
@@ -15,6 +15,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Switch } from './ui/switch';
 import { api } from '@/utils/api';
 import { useToast } from './ui/use-toast';
+import { useEffect } from 'react';
 
 const notificationsFormSchema = z.object({
   reminderEmails: z.boolean(),
@@ -23,67 +24,82 @@ const notificationsFormSchema = z.object({
 type NotificationsFormValues = z.infer<typeof notificationsFormSchema>;
 
 export function SettingsNotifications() {
-  const { toast } = useToast();
   const { data } = api.user.me.useQuery();
-  const { mutate: updateSettings } = api.user.updateConfig.useMutation({});
-
-  const form = useForm<NotificationsFormValues>({
-    resolver: zodResolver(notificationsFormSchema),
-    defaultValues: {
-      reminderEmails: data?.config?.reminderEmails ?? false,
-    },
-  });
-
-  const onSubmit = (data: NotificationsFormValues) => {
-    updateSettings(
-      { ...data },
-      {
-        onSuccess: () => {
-          toast({
-            title: '✅ Success',
-            description: 'Your settings have been updated.',
-          });
-        },
-        onError: (err) => {
-          toast({
-            title: '❌ Error',
-            description: `Error updating settings: ${err.message}`,
-          });
-        },
-      }
-    );
-  };
 
   return (
     <TabsContent value="notifications">
       <Card>
         <CardContent className="space-y-2 pt-4">
-          <Form {...form}>
-            <form className="space-y-8">
-              {data?.config && (
-                <FormField
-                  control={form.control}
-                  name="reminderEmails"
-                  render={({ field }) => (
-                    <FormItem className="space-y-1">
-                      <FormLabel>Reminder emails</FormLabel>
-                      <FormDescription>
-                        We&apos;ll send reminders to your email
-                      </FormDescription>
-                      <FormMessage />
-                      <FormControl
-                        onChange={() => void form.handleSubmit(onSubmit)}
-                      >
-                        <Switch {...field} />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              )}
-            </form>
-          </Form>
+          {data?.config?.reminderEmails !== undefined && (
+            <ReminderSwitch value={data?.config?.reminderEmails} />
+          )}
         </CardContent>
       </Card>
     </TabsContent>
   );
 }
+
+const ReminderSwitch = ({ value }: { value: boolean }) => {
+  const { toast } = useToast();
+  const { mutate: updateSettings } = api.user.updateConfig.useMutation({});
+
+  const form = useForm<NotificationsFormValues>({
+    resolver: zodResolver(notificationsFormSchema),
+    defaultValues: {
+      reminderEmails: value,
+    },
+  });
+
+  const { watch, handleSubmit } = form;
+
+  useEffect(() => {
+    const onSubmit = (data: NotificationsFormValues) => {
+      updateSettings(
+        { ...data },
+        {
+          onSuccess: () => {
+            toast({
+              title: '✅ Success',
+              description: 'Your settings have been updated.',
+            });
+          },
+          onError: (err) => {
+            toast({
+              title: '❌ Error',
+              description: `Error updating settings: ${err.message}`,
+            });
+          },
+        }
+      );
+    };
+    const subscription = watch(() => void handleSubmit(onSubmit)());
+    return () => subscription.unsubscribe();
+  }, [watch, handleSubmit, updateSettings, toast]);
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8">
+        <FormField
+          control={form.control}
+          name="reminderEmails"
+          render={({ field }) => (
+            <FormItem className="flex items-center justify-between space-y-1">
+              <span>
+                <FormLabel>Reminder emails</FormLabel>
+                <FormDescription>
+                  We&apos;ll send reminders to your email
+                </FormDescription>
+              </span>
+              <FormControl>
+                <Switch
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
